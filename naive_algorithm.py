b46f3fa85ed5ea7072ebb19654f17047b301f54b
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-11-13 04:41:35 +0100

    explore basic approach

---
+++ naive_algorithm.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+	"""
+	The function uses nested loop to check for possible pairs of nums that adds up to a target value
+	:param nums: list of numbers
+	:param target: an integer
+	:return: the index if the pair that adds up to the target int value
+	"""
+	for i in range(len(nums)):
+		for j in range(i+1, len(nums)):
+			if nums[i] + nums[j] == target:
+				return [i, j]
+	return[]
