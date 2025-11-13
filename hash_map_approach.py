ca758c6fefc2196f339288f149fb02c9f83e92a5
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-11-13 17:56:01 +0100

    explore samples witht he hash map approach

--- hash_map_approach.py
+++ hash_map_approach.py
@@ -11,3 +11,7 @@ def hash_map_two_sum(nums: List[int], target: int) -> List[int]:
 
 check_sample =hash_map_two_sum([1,3,4,5,6,7,8,8,3,4,3,2], 9)
 print(check_sample)
+print(hash_map_two_sum([1,3,4,5,6,7,8,9,4,3,2], 9))
+print(hash_map_two_sum([1,2,3], 4))
+print(hash_map_two_sum([3,3,4], 6))
+print(hash_map_two_sum([0,3,4], 6))
